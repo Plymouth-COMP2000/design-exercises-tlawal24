bc92723e182467942e187d0af6dcb93c952da6cd
tlawal24 <146185509+tlawal24@users.noreply.github.com>
2026-01-09 14:34:23 +0000

    Add files via upload

---
+++ StaffMenuActivity.java
@@ -0,0 +1,14 @@
+package com.example.androidapp.ui.staff;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.androidapp.R;
+
+public class StaffMenuActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_staff_menu);
+    }
+}
+
