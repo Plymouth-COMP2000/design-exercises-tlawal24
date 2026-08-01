1d1c52ce86ca437dd65405ddcb6804786dc6f119
Tawakalitu Lawal <tawakalitu.lawal@students.plymouth.ac.uk>
2026-08-01 20:20:46 +0100

    Wire Login screen navigation to Member Home.

--- app/src/main/java/com/example/gymfitnessapplication/LoginActivity.java
+++ app/src/main/java/com/example/gymfitnessapplication/LoginActivity.java
@@ -2,6 +2,9 @@ package com.example.gymfitnessapplication;
 
 import android.os.Bundle;
 
+import android.content.Intent;
+import android.widget.Button;
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -15,6 +18,11 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
+        Button loginButton = findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(v -> {
+            Intent intent = new Intent(LoginActivity.this, MemberHomeActivity.class);
+            startActivity(intent);
+        });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
