368c8060c2f0448b735df52c24737907b89f153f
Tawakalitu Lawal <tawakalitu.lawal@students.plymouth.ac.uk>
2026-08-02 14:49:02 +0100

    change the version of the database

--- app/src/main/java/com/example/gymfitnessapplication/DatabaseHelper.java
+++ app/src/main/java/com/example/gymfitnessapplication/DatabaseHelper.java
@@ -7,7 +7,6 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class DatabaseHelper extends SQLiteOpenHelper {
 
     private static final String DATABASE_NAME = "gymfitness.db";
-    private static final int DATABASE_VERSION = 1;
     private static final int DATABASE_VERSION = 2;
 
 
